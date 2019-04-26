6dd9a58a2e90e7592ba43ce09680096f9d3ef249
junnchy <juanichacho@gmail.com>
2019-04-26 19:54:42 -0300

    ejercicio1

--- practico_04/ejercicio01.py
+++ practico_04/ejercicio01.py
@@ -4,59 +4,64 @@
 
 from tkinter import *
 
+nx =20
+ny = 130
 
 def suma():
+ Label(ventana, text= '                                                                            ').place(x=nx, y= ny)
  a = op1.get()
  b = op2.get()
  res = a + b
- etiqueta1 = Label(ventana, text= '%s + %s = %s' % (a, b, res)).place(x=10, y=160)
+ Label(ventana, text= '%s + %s = %s' % (a, b, res), background= 'red').place(x=nx, y= ny)
  print('El resultado: ', res)
  return 0
 
 def resta():
+ Label(ventana, text= '                                                                            ').place(x=nx, y= ny)
  a = op1.get()
  b = op2.get()
  res = a - b
- etiqueta1 = Label(ventana, text= '%s - %s = %s' % (a, b, res)).place(x=10, y=160)
+ Label(ventana, text= '%s - %s = %s' % (a, b, res),background= 'blue').place(x=nx, y=ny)
  print('El resultado: ', res)
- return yres
+ return 0
 
 def multiplica():
+ Label(ventana, text= '                                                                            ').place(x=nx, y= ny)
  a = op1.get()
  b = op2.get()
  res = a * b
- etiqueta1 = Label(ventana, text= '%s * %s = %s' % (a, b, res)).place(x=10, y=160)
+ Label(ventana, text= '%s * %s = %s' % (a, b, res),background= 'green').place(x=nx, y=ny)
  print('El resultado: ', res)
  return 0
 
 def divide():
+ Label(ventana, text= '                                                                            ').place(x=nx, y= ny)
  a = op1.get()
  b = op2.get()
  res = a / b
- etiqueta1 = Label(ventana, text= '%s / %s = %s' % (a, b, res)).place(x=10, y=160)
+ Label(ventana, text= '%s / %s = %s' % (a, b, res),background= 'orange').place(x=nx, y=ny)
  print('El resultado: ', res)
  return 0
+
 
 ventana = Tk()
 
 op1 = IntVar()
 op2 = IntVar()
 
-yres = 260
+ventana.title('Calculadora')
+ventana.geometry('550x200')
+etiqueta1 = Label(ventana, text= 'Primer Operando', ).place(x=10, y=60)
+caja_op1 = Entry(ventana, textvariable= op1, width=12).place(x=20,y=80)
+etiqueta2 = Label(ventana, text= 'Segundo Operando', ).place(x=150, y=60)
+caja_op2 = Entry(ventana, textvariable= op2, width=12).place(x=160,y=80)
 
-ventana.title('Practica 4 - Ejercicio 1')
-ventana.geometry('700x250')
-etiqueta1 = Label(ventana, text= 'Primer Operando', ).place(x=10, y=40)
-caja_op1 = Entry(ventana, textvariable= op1).place(x=10,y=80)
-etiqueta2 = Label(ventana, text= 'Segundo Operando', ).place(x=150, y=40)
-caja_op2 = Entry(ventana, textvariable= op2).place(x=150,y=80)
+but1= Button(ventana, text='+',fg='red', command=suma).place(x=300, y=80)
+but2= Button(ventana, text='-',fg='blue', command=resta).place(x=350, y=80)
+but3= Button(ventana, text='*',fg='green', command=multiplica).place(x=400, y=80)
+but4= Button(ventana, text='/',fg='orange', command=divide).place(x=450, y=80)
 
-but1= Button(ventana, text='+',fg='red', command=suma).place(x=400, y=80)
-but1= Button(ventana, text='-',fg='blue', command=resta).place(x=450, y=80)
-but1= Button(ventana, text='*',fg='green', command=multiplica).place(x=500, y=80)
-but1= Button(ventana, text='/',fg='orange', command=divide).place(x=550, y=80)
-
-but1= Button(ventana, text='Salir',fg='red', command=ventana.quit).place(x=475, y=160)
+but5= Button(ventana, text='Salir',fg='red', command=ventana.quit).place(x=360, y=130)
 
 ventana.mainloop()
 

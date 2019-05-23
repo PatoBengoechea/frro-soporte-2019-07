8f05fa6e39079afc531e15d489d40e861ef52979
Pato Bengoechea <pato.bengoechea@gmail.com>
2019-05-23 12:30:01 -0300

    Practica 3b

--- practico_03-b/ejercicio_03.py
+++ practico_03-b/ejercicio_03.py
@@ -2,19 +2,49 @@
 # Devuelve un booleano en base a si encontro el registro y lo borro o no.
 
 import datetime
+# from practico_03.ejercicio_01 import reset_tabla
+# from practico_03.ejercicio_02 import agregar_persona
 
-from practico_03.ejercicio_01 import reset_tabla
-from practico_03.ejercicio_02 import agregar_persona
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, VARCHAR
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Persona(Base):
+    __tablename__='Persona'
+    IdPersona = Column(Integer, primary_key=True)
+    Nombre = Column(VARCHAR(30))
+    FechaNacimiento = Column(Date)
+    Dni = Column(Integer)
+    Altura = Column(Integer)
+
+
+engine = create_engine('mysql://root:root@localhost:3306/soporte2019')
+Base.metadata.bind = engine
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
 
 
 def borrar_persona(id_persona):
-    return False
+    user = session.query(Persona).filter(Persona.IdPersona == id_persona).delete(synchronize_session=False)
+    session.commit()
+    return user
 
 
-@reset_tabla
-def pruebas():
-    assert borrar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
-    assert borrar_persona(12345) is False
+#
+# @reset_tabla
+# def pruebas():
+#     assert borrar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
+#     assert borrar_persona(12345) is False
+#
+# if __name__ == '__main__':
+#     pruebas()
 
-if __name__ == '__main__':
-    pruebas()
+
+print(borrar_persona(1))
+
+
+#link util https://kite.com/python/docs/sqlalchemy.orm.query.Query.delete
--- practico_03-b/ejercicio_04.py
+++ practico_03-b/ejercicio_04.py
@@ -4,19 +4,45 @@
 
 import datetime
 
-from practico_03.ejercicio_01 import reset_tabla
-from practico_03.ejercicio_02 import agregar_persona
+# from practico_03.ejercicio_01 import reset_tabla
+# from practico_03.ejercicio_02 import agregar_persona
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Date, VARCHAR
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Persona(Base):
+    __tablename__='Persona'
+    IdPersona = Column(Integer, primary_key=True)
+    Nombre = Column(VARCHAR(30))
+    FechaNacimiento = Column(Date)
+    Dni = Column(Integer)
+    Altura = Column(Integer)
+
+
+engine = create_engine('mysql://root:root@localhost:3306/soporte2019')
+Base.metadata.bind = engine
+DBSession = sessionmaker()
+DBSession.bind = engine
+session = DBSession()
 
 
 def buscar_persona(id_persona):
+    user = session.query(Persona).filter(Persona.IdPersona == id_persona)
+    print(user)
     return False
 
 
-@reset_tabla
-def pruebas():
-    juan = buscar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
-    assert juan == (1, 'juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
-    assert buscar_persona(12345) is False
+# @reset_tabla
+# def pruebas():
+#     juan = buscar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
+#     assert juan == (1, 'juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
+#     assert buscar_persona(12345) is False
+#
+# if __name__ == '__main__':
+#     pruebas()
 
-if __name__ == '__main__':
-    pruebas()
+buscar_persona(2)
